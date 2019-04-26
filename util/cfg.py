81e26d7dcd9ec992530bc47d5b2d6b57b37ccb84
LucasX <xulu0620@gmail.com>
2019-04-26 09:03:48 +0800

    fix a bug

--- util/cfg.py
+++ util/cfg.py
@@ -2,8 +2,9 @@ from collections import OrderedDict
 
 config = OrderedDict()
 config['scut_fbp_base'] = '/home/xulu/DataSet/Face/SCUT-FBP'
+config['test_ratio'] = 0.2
 config['label_excel_path'] = '/home/xulu/DataSet/Face/SCUT-FBP/Rating_Collection/AttractivenessLabel.xlsx'
-config['face_image_filename'] = '/home/xulu/DataSet/Face/SCUT-FBP/Processed/SCUT-FBP-{0}.jpg'
+config['face_image_filename'] = '/home/xulu/DataSet/Face/SCUT-FBP/Crop/SCUT-FBP-{0}.jpg'
 config['predictor_path'] = "/media/lucasx/Document/ModelZoo/shape_predictor_68_face_landmarks.dat"
 config['vgg_face_model_mat_file'] = '/home/xulu/ModelZoo/vgg-face.mat'
 config['eccv_dataset_attribute'] = '../preprocess/eccv_face_attribute.csv'
