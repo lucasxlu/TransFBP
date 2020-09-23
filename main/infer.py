95795cf25c24cef42417108a13614fe53d0e52a1
LucasX <xulu0620@gmail.com>
2020-09-23 22:34:48 +0800

    update infer.py

--- main/infer.py
+++ main/infer.py
@@ -1,27 +1,32 @@
-import numpy as np
-import cv2
+# inference demo
 import sys
 import time
+import os
 
-from sklearn import linear_model
+import cv2
+from sklearn.externals import joblib
 
 sys.path.append('../')
 from util.vgg_face_feature import extract_conv_feature
 
+BAYES_RIDGE_REG_WEIGHTS = './model/BayesRidge_SCUT-FBP5500.pkl'
+assert os.path.exists(BAYES_RIDGE_REG_WEIGHTS)
 
-def infer_from_img(img_file):
+
+def infer_from_img(model, img_file):
+    """
+    infer from image with a given model
+    """
     img = cv2.imread(img_file)
     img = cv2.resize(img, (224, 224))
-    # tik = time.time()
-    feat = extract_conv_feature(img, layer_name='conv5_1').tolist()
-    reg = linear_model.BayesianRidge(np.array(feat))
-    reg.fit(np.random.rand(1, len(feat)), np.array(1))
+    feat = extract_conv_feature(img, layer_name='fc6').tolist() + extract_conv_feature(img, layer_name='fc7').tolist()
     tik = time.time()
-    score = reg.predict(feat)
+    score = model.predict(feat)
     tok = time.time()
 
-    print('Beauty score is {0}, it takes {1} seconds!'.format(score, (tok - tik) * 1000))
+    print('Beauty score is {0}, it takes {1} seconds!'.format(score, tok - tik))
 
 
 if __name__ == '__main__':
-    infer_from_img('../testimg.jpg')
+    bayes_ridge_reg = joblib.load(BAYES_RIDGE_REG_WEIGHTS)
+    infer_from_img(bayes_ridge_reg, '../testimg.jpg')
